bea45fdf18e370e443b0f36f0a34e05a03e54040
kylrth <kylrth@gmail.com>
2019-02-05 09:09:22 -0700

    formatting standardization

--- __init__.py
+++ __init__.py
@@ -1,12 +1,7 @@
-"""
-Here we put subpackages. For example, without running this code
-you would have to run `from speech2phone import pca`. 
-After running this code, we can call 
-`import speech2phone; speech2phone.pca.foo()`.
-This difference is why you can call 
-`import numpy as np; np.linalg.solve()` just fine, but
-`import scipy as sp; sp.linalg.solve()` doesn't work.
-You have to call `import from scipy import linalg`.
+"""Here we put subpackages. For example, without running this code you would have to run `from speech2phone import pca`.
+After running this code, we can call `import speech2phone; speech2phone.pca.foo()`. This difference is why you can call
+`import numpy as np; np.linalg.solve()` just fine, but `import scipy as sp; sp.linalg.solve()` doesn't work. You have to
+call `from scipy import linalg`.
 """
 
 # TL;DR: Do this for every folder, along with __init__.py
--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,8 +1,8 @@
-"""
-Everything imported here will be available under `speech2phone.preprocessing`.
-So even though the function `test_preprocess` is in the filesystem as
-`speech2phone/preprocessing/preprocessing.py -> test_preprocess`,
-it is available in Python as `speech2phone.preprocessing.test_preprocess`.
+"""Everything imported here will be available under `speech2phone.preprocessing`. So even though the function
+`test_preprocess` is in the filesystem as `speech2phone/preprocessing/preprocessing.py -> test_preprocess`, it is
+available in Python as `speech2phone.preprocessing.test_preprocess`.
+
+Jared Nielsen. 2019-02-05.
 """
 
 # TL;DR: Put everything here. Remember the dot.
